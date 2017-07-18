321680b69b1823c5f186899ffdd6ae267fe2a94d
jonathansun5 <jonathansun5@berkeley.edu>
2017-07-17 17:37:55 -0700

    changed back to old contact but changed email to gmail instead

--- Jonathan_Sun_Website/contact.php
+++ Jonathan_Sun_Website/contact.php
@@ -1,24 +1,69 @@
 <?php
-// Check for empty fields
-if(empty($_POST['name'])  		||
-   empty($_POST['email']) 		||
-   empty($_POST['comment']) 	||
-   !filter_var($_POST['email'],FILTER_VALIDATE_EMAIL))
-   {
-	echo "No arguments Provided!";
-	return false;
+
+//Retrieve form data. 
+//GET - user submitted data using AJAX
+//POST - in case user does not support javascript, we'll use POST instead
+$name = ($_GET['name']) ? $_GET['name'] : $_POST['name'];
+$email = ($_GET['email']) ?$_GET['email'] : $_POST['email'];
+$comment = ($_GET['comment']) ?$_GET['comment'] : $_POST['comment'];
+
+//flag to indicate which method it uses. If POST set it to 1
+
+if ($_POST) $post=1;
+
+//Simple server side validation for POST data, of course, you should validate the email
+if (!$name) $errors[count($errors)] = 'Please enter your name.';
+if (!$email) $errors[count($errors)] = 'Please enter your email.'; 
+if (!$comment) $errors[count($errors)] = 'Please enter your message.'; 
+
+//if the errors array is empty, send the mail
+if (!$errors) {
+
+   //recipient - replace your email here
+   $to = 'jonathansun5@gmail.com';  
+   //sender - from the form
+   $from = $name . ' <' . $email . '>';
+   
+   //subject and the html message
+   $subject = 'Message via Personal Website from ' . $name; 
+   $message = 'Name: ' . $name . '<br/><br/>
+             Email: ' . $email . '<br/><br/>    
+             Message: ' . nl2br($comment) . '<br/>';
+
+   //send the mail
+   $result = sendmail($to, $subject, $message, $from);
+   
+   //if POST was used, display the message straight away
+   if ($_POST) {
+      if ($result) echo 'Thank you! We have received your message.';
+      else echo 'Sorry, unexpected error. Please try again later';
+      
+   //else if GET was used, return the boolean value so that 
+   //ajax script can react accordingly
+   //1 means success, 0 means failed
+   } else {
+      echo $result;  
    }
-	
-$name = $_POST['name'];
-$email_address = $_POST['email'];
-$comment = $_POST['comment'];
-	
-// Create the email and send the message
-$to = 'jonathansun5@berkeley.edu'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
-$email_subject = "Website Contact Form:  $name";
-$email_body = "You have received a new message from your website contact form.\n\n"."Here are the details:\n\nName: $name\n\nEmail: $email_address\n\nMessage:\n$comment";
-$headers = "From: noreply@yourdomain.com\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
-$headers .= "Reply-To: $email_address";	
-mail($to,$email_subject,$email_body,$headers);
-return true;			
+
+//if the errors array has values
+} else {
+   //display the errors message
+   for ($i=0; $i<count($errors); $i++) echo $errors[$i] . '<br/>';
+   echo '<a href="index.html">Back</a>';
+   exit;
+}
+
+
+//Simple mail function with HTML header
+function sendmail($to, $subject, $message, $from) {
+   $headers = "MIME-Version: 1.0" . "\r\n";
+   $headers .= "Content-type:text/html;charset=iso-8859-1" . "\r\n";
+   $headers .= 'From: ' . $from . "\r\n";
+   
+   $result = mail($to,$subject,$message,$headers);
+   
+   if ($result) return 1;
+   else return 0;
+}
+
 ?>
